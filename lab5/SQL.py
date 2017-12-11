6f41505bf0a380150533e0152a1f9f272ea8d006
romanchubak <romanchubak@gmail.com>
2017-12-12 00:43:36 +0200

    lab5

---
+++ lab5/SQL.py
@@ -0,0 +1,39 @@
+import sqlite3 as lite
+class SQL:
+    def __init__(self, name):
+        self.con = lite.connect(name)
+
+    def __del__(self):
+        self.con.close()
+
+    def init_DB(self):
+        self.con.execute("DROP TABLE IF EXISTS Book;")
+        self.con.execute('''CREATE TABLE Book
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT    NOT NULL,
+					first_name_author CHAR(50)NOT NULL,
+					second_name_author CHAR(50)NOT NULL,
+					name CHAR(50) NOT NULL,
+					number_of_page INT,
+					year_of_publication INT NOT NULL,
+					name_of_publishing_house CHAR(50),
+					date_of_arrival DATE);''')
+        self.con.commit()
+
+    def add(self, book):
+        self.con.execute("INSERT INTO Book (first_name_author, second_name_author, name, number_of_page, year_of_publication, name_of_publishing_house, date_of_arrival) VALUES (?, ?, ?, ?, ?, ?, ? )",
+                                            (book.first_name_author, book.second_name_author,book.name,book.number_of_page,book.year_of_publication,book.name_of_publishing_house,book.date_of_arrival));
+        self.con.commit()
+
+    def count_unique_authors(self):
+        cursor =  self.con.execute("SELECT COUNT(DISTINCT second_name_author) FROM Book;")
+        return cursor.fetchone()
+
+    def print(self):
+        cursor = self.con.execute("SELECT * FROM Book;")
+        data = cursor.fetchall()
+        print(data)
+
+    def find_oldest_book(self):
+        cursor = self.con.execute("SELECT name FROM Book ORDER BY year_of_publication DESC LIMIT 1;")
+        return cursor.fetchone()
+
--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,1 +1,62 @@
+import ZODB
+import ZODB.FileStorage
+from SQL import SQL
+from datetime import date
+import transaction
+
+class Book(object):
+    def __init__(self, first_name_author = "first_name_author", second_name_author = "second_name",name = "book", number_of_page = 0, year_of_publication = 1900,name_of_publishing_house = "vydavnyctvo", date_of_arrival = date.today()):
+        self.first_name_author = first_name_author
+        self.second_name_author = second_name_author
+        self.name = name
+        self.number_of_page = number_of_page
+        self.year_of_publication = year_of_publication
+        self.name_of_publishing_house = name_of_publishing_house
+        self.date_of_arrival = date_of_arrival
+
+
+def find_oldest_book(arr):
+    oldest = Book(year_of_publication=2018)
+    for a in arr:
+        if oldest.year_of_publication > a.year_of_publication:
+            oldest = a
+    return oldest
+       
+
+n = int(input("input number of books"))
+books = []
+for i in range(0,n):
+   first_name_author = input("input first name of auther")
+   second_name_author = input("input first name of auther")
+   name = input("input first name of book")
+   number_of_page = int(input("input number of page"))
+   year_of_publication = int(input("input year of publication"))
+   name_of_publishing_house = input("input first name of publication house")
+   date_of_arrival = date.today()
+
+
+   
+storage = ZODB.FileStorage.FileStorage("mydata.fs")
+db = ZODB.DB(storage)
+connection = db.open()
+root = connection.root
+
+root.items = []
+for book in books:
+    root.append(book)
+
+transaction.commit()
+print("oldest book in zodb")
+print(find_oldest_book(root.items).name)
+
+
+sql = SQL("test.sqlite")
+sql.init_DB()
+for book in books:
+    sql.add(book)
+
+print("oldest book in sql")
+print(sql.find_oldest_book())
+print("count of unique authors")
+print(sql.count_unique_authors())
 

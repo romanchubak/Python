3711e1e1f8d377d6684a62648e869be172987910
Roman <romanchubak@gmail.com>
2017-12-12 13:43:08 +0200

    lab7

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,16 +1,24 @@
 from tkinter import *
-from collections import Counter
-def func(var):
-    word = var.get()
-    count_A = 0
-    count_B = 0
 
-    for (key, value) in Counter(word).items():
-        if value.isalpha():
-            count_A+=1
-        else :
-            count_B+=1
-    L1['text'] = "Count alpha : " + str(count_A)  + "count not alpha: " + str(count_B)
+def isVowel(char):
+    return char.lower() in 'aeiou'
+
+def func(var):
+	word = var.get()
+	count_A = 0
+	count_B = 0
+	for ch in word:
+		if isVowel(ch) and ch.isalpha():
+			count_A+=1
+		else :
+			count_B+=1
+	output = "Count: "
+	count = 0
+	if vowelON.get():
+		 count += count_A
+	if consonantON.get():
+		count += count_B
+	L1['text'] = output + str(count)
 
 
 top = Tk()
@@ -18,21 +26,21 @@ L1 = Label(top, text="Count: 0")
 L1.pack(side=TOP)
 
 
-sv = StringVar()
-sv.trace('w', lambda nm, idx, mode, var=sv: func(var))
+word = StringVar()
+word.trace('w', lambda nm, idx, mode, var=word: func(var))
 
-input = Entry(top, bd=5, textvariable=sv)
+input = Entry(top, bd=5, textvariable=word)
 input.pack(side=LEFT)
 
-bVar_1 = BooleanVar()
-bVar_1.trace('w', lambda nm, idx, mode, var=sv: func(var))
+vowelON = BooleanVar()
+vowelON.trace('w', lambda nm, idx, mode, var=word: func(var))
 
-bVar_2 = BooleanVar()
-bVar_2.trace('w', lambda nm, idx, mode, var=sv: func(var))
+consonantON = BooleanVar()
+consonantON.trace('w', lambda nm, idx, mode, var=word: func(var))
 
 
-radio1 = Checkbutton(top, text="2", variable=bVar_1)
-radio2 = Checkbutton(top, text="+", variable=bVar_2)
+radio1 = Checkbutton(top, text="vowel", variable=vowelON)
+radio2 = Checkbutton(top, text="consonant", variable=consonantON)
 radio1.pack(side=BOTTOM)
 radio2.pack(side=BOTTOM)
 top.mainloop()
